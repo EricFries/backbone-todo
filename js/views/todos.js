d4a4da85d0e7dfa9b4a2df9f06f39468b321f0d5
EricFries <ericfries@gmail.com>
2015-09-20 16:46:11 -0400

    finish

--- js/views/todos.js
+++ js/views/todos.js
@@ -12,7 +12,9 @@ app.TodoView = Backbone.View.extend({
 
   // The DOM events specific to an item.
   events: {
+    'click .toggle': 'togglecompleted',
     'dblclick label': 'edit',
+    'click .destroy': 'clear',
     'keypress .edit': 'updateOnEnter',
     'blue .edit': 'close'
   },
@@ -22,20 +24,47 @@ app.TodoView = Backbone.View.extend({
   // app, we set a direct reference on the model for convenience.
   initialize: function(){
     this.listenTo(this.model, 'change', this.render);
+    this.listenTo(this.model, 'destroy', this.remove);
+    this.listenTo(this.model, 'visible', this.toggleVisible);
   },
 
   render: function(){
     this.$el.html( this.template( this.model.attributes ) );
+    this.$el.toggleClass( 'completed', this.model.get('completed') );
+    this.toggleVisible();
     this.$input = this.$('.edit');
     return this;
   },
 
-  //Close the '"editing"' mode, saving hanges to the todo.
+  toggleVisible: function(){
+    this.$el.toggleClass( 'hidden', this.isHidden() );
+  },
+
+  isHidden: function(){
+    var isCompleted = this.model.get('completed');
+    return (//hidden cases only
+      (!isCompleted && app.TodoFilter === 'completed') || (isCompleted && app.TodoFilter === 'active')
+      );
+  },
+
+  togglecompleted: function(){
+    this.model.toggle();
+  },
+
+  edit: function(){
+    this.$el.addClass('editing');
+    this.$input.focus();
+  },
+
+  //Close the '"editing"' mode, saving changes to the todo.
   close: function(){
     var value = this.$input.val().trim();
 
     if ( value ){
       this.model.save({ title: value });
+    }
+    else {
+      this.clear();
     }
     this.$el.removeClass('editing');
   },
@@ -46,5 +75,9 @@ app.TodoView = Backbone.View.extend({
     if ( event.whcih === ENTER_KEY ){
       this.close();
     }
+  },
+
+  clear: function(){
+    this.model.destroy();
   }
 });
